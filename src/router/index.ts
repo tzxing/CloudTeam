99aaedb5d95cf916e755e8406ca63b325bd4c7be
WANG Guan <netspecters@126.com>
2019-10-29 12:35:51 +0800

    增加登陆路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store';
 
 Vue.use(VueRouter)
 
@@ -11,19 +12,29 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.path === "/")
+      (store.state as any).user.access_token = "";
+
+  if ((to.path !== "/" && to.path !== "/register" && to.path !== "/finished")
+      && (store.state as any).user.access_token === "") {
+      console.warn("强制转跳到根页");
+      next("/");
+  } else {
+      next();
+  }
+});
 
 export default router
