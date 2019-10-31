c271f803e9793dd11ac396004d9b92d3beb41bfb
WANG Guan <netspecters@126.com>
2019-10-31 17:19:24 +0800

    增加工作流模块

--- src/router/internal.ts
+++ src/router/internal.ts
@@ -3,4 +3,10 @@ export default [{
     path: '/internal',
     name: 'internal',
     component: () => import("@/views/InternalViews/Index.vue"),
+    children: [
+        {
+            path: 'workflow', name: 'workflow',
+            component: () => import("@/views/InternalViews/Workflow/Index.vue"),
+        },
+    ],
 }]
