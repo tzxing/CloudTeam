fbcde139eb4d0e80abd8b23d1790a1fc45aa674e
WANG Guan <netspecters@126.com>
2019-10-31 17:25:52 +0800

    在internal域内不使用child路由 为了给各子模块更大路由权限，在internal域内使用直接路由

--- src/router/internal.ts
+++ src/router/internal.ts
@@ -1,24 +1,23 @@
 
-export default [{
-    path: '/internal',
-    name: 'internal',
-    component: () => import("@/views/InternalViews/Index.vue"),
-    children: [
-        {
-            path: 'workflow', name: 'workflow',
-            component: () => import("@/views/InternalViews/Workflow/Index.vue"),
-        },
-        {
-            path: 'smartcar', name: 'smartcar',
-            component: () => import("@/views/InternalViews/SmartCar/Index.vue"),
-        },
-        {
-            path: 'parkinson', name: 'parkinson',
-            component: () => import("@/views/InternalViews/ParkinsonMedical/Index.vue"),
-        },
-        {
-            path: 'deep_space', name: 'deep_space',
-            component: () => import("@/views/InternalViews/DeepSpace/Index.vue"),
-        },
-    ],
-}]
+export default [
+    {
+        path: '/internal', name: 'internal',
+        component: () => import("@/views/InternalViews/Index.vue"),
+    },
+    {
+        path: '/internal/workflow', name: 'workflow',
+        component: () => import("@/views/InternalViews/Workflow/Index.vue"),
+    },
+    {
+        path: '/internal/smartcar', name: 'smartcar',
+        component: () => import("@/views/InternalViews/SmartCar/Index.vue"),
+    },
+    {
+        path: '/internal/parkinson', name: 'parkinson',
+        component: () => import("@/views/InternalViews/ParkinsonMedical/Index.vue"),
+    },
+    {
+        path: '/internal/deep_space', name: 'deep_space',
+        component: () => import("@/views/InternalViews/DeepSpace/Index.vue"),
+    },
+]
