bfbf62acd64a99e41bf53fa58dbe64874f8c6d17
WANG Guan <netspecters@126.com>
2019-10-31 08:44:34 +0800

    增加登陆界面

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,15 @@
 <template>
-    <div>
-        
-    </div>
+  <div class="login-container">
+    <el-card id="login-components">
+      <el-input v-model="username" placeholder="用户名"></el-input>
+      <el-input v-model="password" placeholder="密码"></el-input>
+      <el-checkbox v-model="remember_me" style="align-self: flex-start;">记住登陆</el-checkbox>
+      <div id="buttons">
+        <el-button type="primary" @click="do_login">登陆</el-button>
+        <el-button>注册</el-button>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 
@@ -10,10 +18,49 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class LoginView extends Vue {
-    
+  private username: string = "";
+  private password: string = "";
+  private remember_me: boolean = false;
+
+  public do_login() {
+    this.$router.replace("/internal");
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.login-container {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0px;
+  bottom: 0px;
+  background-image: url("../assets/login-background.jpg");
+  background-size: 100% 100%;
 
+  #login-components {
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
+    right: 5vw;
+    width: 290px;
+    padding: 30px 20px;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .el-input,
+    .el-checkbox {
+      margin-bottom: 15px;
+    }
+
+    #buttons {
+      width: 100%;
+      display: grid;
+      grid-template-columns: 50% 50%;
+    }
+  }
+}
 </style>
