fdda70c689cb0f248d4a7b58593e3f437b6b1308
WANG Guan <netspecters@126.com>
2019-12-08 20:31:32 +0800

    初始化wfc node

---
+++ src/views/InternalViews/Workflow/Components/WorkflowChartNode.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wf-node">
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { jsPlumbInstance } from "jsplumb";
+
+@Component({
+  components: { WorkflowChartNode }
+})
+export default class WorkflowChartNode extends Vue {
+  @Prop({ required: true, type: String })
+  public id!: string;
+
+  @Prop({ required: true })
+  public jsp_instance!: jsPlumbInstance;
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
