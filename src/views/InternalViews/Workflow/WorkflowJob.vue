fa79f56ebf9acecd2fa56241e00bd44c81dadff4
马许 <maxu@maxudeMacBook-Air-2.local>
2019-12-19 00:33:14 +0800

    change name

--- src/views/InternalViews/Workflow/WorkflowJob.vue
+++ src/views/InternalViews/Workflow/WorkflowJob.vue
@@ -1,83 +1,17 @@
 <template>
-  <div class="container">
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column label="名称" width="200">
-      <template slot-scope="scope">
-        <span style="margin-left: 10px">{{ scope.row.workflow_name }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column label="执行日期" width="200">
-      <template slot-scope="scope">
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ scope.row.creation_timestamp }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column label="状态" width="180">
-      <template slot-scope="scope">
-        <div slot="reference" class="name-wrapper">
-          <el-tag size="medium">{{ scope.row.status }}</el-tag>
-        </div>
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button size="mini" @click="handleEdit(scope.row.workflow_name)">详情</el-button>
-        <el-button size="mini" type="danger" @click="selectWorkflow(scope.row.workflow_name)">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-  <el-dialog
-          title="提示"
-          :visible.sync="dialogVisible"
-          width="30%"
-        >
-          <span>删除后无法恢复，确认删除吗？</span>
-          <span slot="footer" class="dialog-footer">
-            <el-button @click="dialogVisible = false">取 消</el-button>
-            <el-button type="primary" @click="handleDelete()">确 定</el-button>
-          </span>
-        </el-dialog>
+  <div>
+    <WorkflowChart  chart_data='[{"name":"A","dependencies":[],"id":"1","template":"alpine: 3.7"},{"name":"B","id":"2","dependencies":["A"],"template":"alpine: 3.7"},{"name":"C","dependencies":["A"],"id":"3","template":"alpine: 3.7"},{"name":"D","id":"4","dependencies":["B","C"],"template":"alpine: 3.7"}]'></WorkflowChart>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-@Component({})
-export default class WorkflowJobView extends Vue {
-  tableData = [];
-  dialogVisible = false;
-  workflowName = "";
-  created() {
-    this.getData();
-  }
-  async getData() {
-    try {
-      const { data } = await this.$axios.get("wfs/workflowJobs");
-      console.log(data);
-      this.tableData = data;
-    } catch (e) {
-      this.$message.error("请求用户数据失败，请稍后再试！");
-    }
-  }
+import WorkflowChart from "./Components/WorkflowChart.vue";
 
-  selectWorkflow(row: string) {
-    this.workflowName = row;
-    this.dialogVisible = true;
-  }
+@Component({
+    components:{WorkflowChart}
+})
+export default class DashboardView extends Vue {
 
-  handleEdit(row: string) {}
-
-  async handleDelete() {
-    console.log(this.workflowName)
-    try {
-      const { data } = await this.$axios.delete("wfs/workflowJobs/" + this.workflowName,);
-      this.tableData = this.tableData.filter(
-        item => item["workflow_name"] != this.workflowName
-      );
-    } catch (e) {
-      this.$message.error("请求用户数据失败，请稍后再试！");
-    }
-    this.dialogVisible = false
-  }
 }
 </script>
