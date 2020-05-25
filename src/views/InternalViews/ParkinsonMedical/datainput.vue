1c4031440ebf39940a5dff67990cf5a2e1952406
zhuyanbo <yanbo_0605z@163.com>
2020-05-25 08:00:59 +0800

    信息补全页面修复bug

--- src/views/InternalViews/ParkinsonMedical/datainput.vue
+++ src/views/InternalViews/ParkinsonMedical/datainput.vue
@@ -14,37 +14,37 @@
             <el-divider>个人信息</el-divider>
             <el-form-item label="用户昵称">
                 <el-col :span="8">
-                <el-input v-model="form.user.nickname" :disabled="false" placeholder="请输入用户昵称" @input="change($event)" />
+                <el-input v-model="form.user.nickname" :disabled="false" placeholder="请输入用户昵称" />
                 </el-col>
             </el-form-item>
             <el-form-item label="姓名">
                 <el-col :span="8">
-                <el-input v-model="form.user.name" :disabled="false" placeholder="请输入姓名" @input="change($event)" />
+                <el-input v-model="form.user.name" :disabled="false" placeholder="请输入姓名" />
                 </el-col>
             </el-form-item>
             <el-form-item label="手机号">
                 <el-col :span="8">
-                <el-input v-model="form.user.phone" :disabled="false" placeholder="请输入手机号" @input="change($event)" />
+                <el-input v-model="form.user.phone" :disabled="false" placeholder="请输入手机号" />
                 </el-col>
             </el-form-item>
             <el-form-item label="身份证号">
                 <el-col :span="8">
-                <el-input v-model="form.user.idcard" :disabled="false" placeholder="请补全身份证信息" @input="change($event)" />
+                <el-input v-model="form.user.idcard" :disabled="false" placeholder="请补全身份证信息" />
                 </el-col>
             </el-form-item>
             <el-form-item label="省份">
                 <el-col :span="8">
-                <el-input v-model="form.user.province" :disabled="false" placeholder="请输入居住省份" @input="change($event)" />
+                <el-input v-model="form.user.province" :disabled="false" placeholder="请输入居住省份" />
                 </el-col>
             </el-form-item>
             <el-form-item label="城市">
                 <el-col :span="8">
-                <el-input v-model="form.user.city" :disabled="false" placeholder="请输入居住城市" @input="change($event)" />
+                <el-input v-model="form.user.city" :disabled="false" placeholder="请输入居住城市" />
                 </el-col>
             </el-form-item>
             <el-form-item label="地址">
                 <el-col :span="12">
-                <el-input v-model="form.user.address" :disabled="false" placeholder="请输入居住的具体地址" @input="change($event)" />
+                <el-input v-model="form.user.address" :disabled="false" placeholder="请输入居住的具体地址" />
                 </el-col>
             </el-form-item>
             <el-divider>身份信息</el-divider>
@@ -56,8 +56,15 @@
             </el-form-item>
             <el-form-item label="验证码">
                 <el-col :span="8">
-                <el-input v-model="form.user.vericode" :disabled="form.user.identity" :placeholder="form.message" @input="change($event)" />
+                <el-input v-model="form.user.vericode" :disabled="form.user.identity" :placeholder="form.message" />
                 </el-col>
+            </el-form-item>
+            
+
+            <el-form-item>
+                <el-button type="primary" @click="doLogin">
+                完成
+                </el-button>
             </el-form-item>
             </el-form >
               <el-button type="primary" @click="doLogin" style="float:right; margin-right:350px" >
@@ -70,6 +77,8 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator"
+
+@Component({})
 
 export default class datainput extends Vue {
   private form= {
@@ -89,12 +98,11 @@ export default class datainput extends Vue {
   };
 
   public change(e:any) {
-    if (this.form.user.identity == true) {
+    if (this.form.user.identity == true){
       this.form.user.vericode = '';
       this.form.message = '无需输入验证码';
     }
     else this.form.message = '请输入验证码';
-    this.$forceUpdate()
   }
   public doLogin() {
     //需要与后端交互
