d8bf94c5ab818f3bdebd9788e0a7341b5f08e662
ZCY <409997138@qq.com>
2021-01-11 22:23:26 +0800

    fix：注释mock

--- src/AxiosPlugin.ts
+++ src/AxiosPlugin.ts
@@ -12,16 +12,16 @@ export const Axios = axios.create({ baseURL: "/api", timeout: 10000 });
 
 
 //mock 模拟数据
- const MockAdapter = require("axios-mock-adapter");
- const mock = new MockAdapter(Axios);
+// const MockAdapter = require("axios-mock-adapter");
+ //const mock = new MockAdapter(Axios);
  //模拟工作流列表
- mock.onGet("/api/wfs/workflow_list/WFFInfo").reply(200,[{'wf_id':"a", 'date':"2020-9-1",'name':"zc"}]);
+ /*mock.onGet("/api/wfs/workflow_list/WFFInfo").reply(200,[{'wf_id':"a", 'date':"2020-9-1",'name':"zc"}]);
  mock.onGet("/api/wfs/workflow_list/WFTInfo").reply(200,[{'wf_id':"b", 'date':"2020-9-1",'name':"zc1"}]);
  mock.onPost("/api/wfs/workflows/a/execute").reply(200,"zc");
  mock.onGet("/api/wfs/workflowJobs/zc").reply(200,'{[{"name": "A", "dependencies": {[]}, "id": "1", "template": "alpine: 3.7", "style_type": "success"}, {"name": "B", "dependencies": {["A"]}, "id": "2", "template": "alpine: 3.7", "style_type": "normal"}, {"name": "C", "dependencies": {["A"]}, "id": "3", "template": "alpine: 3.7", "style_type": "disable"}, {"name": "D", "dependencies": {["B", "C"]}, "id": "4", "template": "alpine: 3.7", "style_type": "success"}]}');
 //模拟登录
 mock.onPost("/api/users/login").reply(200,{access_token:"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEyMyIsInVzZXJuYW1lIjoiaGFoYSIsImVtYWlsIjoiMTIzIiwicm9sZSI6MX0.SPjFd-QZ9TG9QHdA0_3Dz4hclw0PRUOBOqP401IlXQI"
-,username:"管理员",stuid:"3220191001",role:"admin"});
+,username:"管理员",stuid:"3220191001",role:"admin"});*/
 // 在发送请求之前做某件事
 Axios.interceptors.request.use(config => {
     if ((store.state as any).user.access_token !== "") {
